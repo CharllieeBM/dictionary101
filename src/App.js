c33138bb6d8e31c04d15f57892b3f27292fcb9e1
CharllieeBM <cburylb2@gmail.com>
2025-12-16 12:44:10 +0200

    tested if it works and fixed a few css styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Hello from App!
+      <header className="App-header">
+        Hello from App!
+      </header>
     </div>
   );
 }
